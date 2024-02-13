d3b8fb4a4627e75047e9ee39039dfead32ee879a
eazelis <95705759+eazelis@users.noreply.github.com>
2024-02-13 06:07:36 +0000

    GUI full files

---
+++ gui/6040CEM_GUI_Windows/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using _6040CEM_GUI_Windows.View;
+
+namespace _6040CEM_GUI_Windows
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+
+}
